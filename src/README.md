670c0bab603b74fcbef9860921705b4f0b6cfe9b
Cullen S <cullensmi@gmail.com>
2020-11-21 18:50:11 -0500

    Joining threads

--- src/README.md
+++ src/README.md
@@ -9,5 +9,6 @@ Web servers can serve several clients at the same time using several threads, i.
 JVM has what is known as a thread scheduler, the job of this is to decide what threads to run and for how long. 
 So if you have more tasks than available threads, the scheduler switches between tasks.  
 
-
+When we are running multiple processes, i.e. downloading a file, and we want something to run after a file is downloaded it is 
+better to join threads.
 
--- src/com/csmithswim/ThreadDemo.java
+++ src/com/csmithswim/ThreadDemo.java
@@ -5,12 +5,14 @@ public class ThreadDemo {
         Thread thread = new THread(new DownloadFileTask());
         thread.start();
 
+        try {
+            //Tells the current thread to wait for the current thread to finish its processes
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-
-
-
-
-
+        System.out.println("File is ready to be scanned.");
 
         //Printing the main thread, each thread has a name and an ID
         System.out.println(Thread.currentThread().getName());
